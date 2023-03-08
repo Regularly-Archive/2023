a7337b5ccb775a2841353dbf1472f9ceb93f089b
qinyuanpei <qinyuanpei@163.com>
2023-03-08 08:39:21 +0800

    增加 Miservice

---
+++ xiaoi-gpt/miservice/__init__.py
@@ -0,0 +1,6 @@
+from .miaccount import MiAccount, MiTokenStore
+from .minaservice import MiNAService
+from .miioservice import MiIOService
+from .miiocommand import miio_command, miio_command_help
+
+MISERVICE_VERSION = "2.0.2"
