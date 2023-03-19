b8e029299401668e5f93b1aa8f6dc07a832f7fc9
qinyuanpei <qinyuanpei@163.com>
2023-03-19 22:35:55 +0800

    新玩具

---
+++ Jarvis/main.py
@@ -0,0 +1,50 @@
+import speech_recognition as sr
+import pyttsx3
+import cv2
+
+# 初始化语音识别和语音合成客户端
+r = sr.Recognizer()
+engine = pyttsx3.init()
+
+# 语音识别函数
+def recognize_speech():
+    with sr.Microphone() as source:
+        print('Please speak:')
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio, language='zh-CN')
+        print('You said:', text)
+        return text
+    except Exception as e:
+        print('Error:', e)
+        return ''
+
+# 语音合成函数
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+# 图像识别函数
+def recognize_image():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+# 主程序
+speak('你好，请问有什么可以帮助您的？')
+while True:
+    text = recognize_speech()
+    if text == '退出':
+        break
+    elif text == '打开相机':
+        speak('好的，请看摄像头')
+        recognize_image()
+    elif text == '你叫什么名字':
+        speak('我是小助手')
+    else:
+        speak('对不起，我不明白您的意思，请您再说一遍。')
