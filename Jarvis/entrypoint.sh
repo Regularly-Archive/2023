039ec2c88ca1d483dc8f073627e6a747f684982f
qinyuanpei <qinyuanpei@163.com>
2023-05-04 08:19:27 +0800

    更新脚本

---
+++ Jarvis/entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Fail on errors.
+set -e
+
+# Make sure .bashrc is sourced
+. /root/.bashrc
+
+# Allow the workdir to be set using an env var.
+# Useful for CI pipiles which use docker for their build steps
+# and don't allow that much flexibility to mount volumes
+SRCDIR=$1
+
+PYPI_URL=$2
+
+PYPI_INDEX_URL=$3
+
+WORKDIR=${SRCDIR:-/src}
+
+SPEC_FILE=${4:-*.spec}
+
+python -m pip install --upgrade pip wheel setuptools
+
+#
+# In case the user specified a custom URL for PYPI, then use
+# that one, instead of the default one.
+#
+if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
+   [[ "$PYPI_INDEX_URL" != "https://pypi.python.org/simple" ]]; then
+    # the funky looking regexp just extracts the hostname, excluding port
+    # to be used as a trusted-host.
+    mkdir -p /wine/drive_c/users/root/pip
+    echo "[global]" > /wine/drive_c/users/root/pip/pip.ini
+    echo "index = $PYPI_URL" >> /wine/drive_c/users/root/pip/pip.ini
+    echo "index-url = $PYPI_INDEX_URL" >> /wine/drive_c/users/root/pip/pip.ini
+    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /wine/drive_c/users/root/pip/pip.ini
+
+    echo "Using custom pip.ini: "
+    cat /wine/drive_c/users/root/pip/pip.ini
+fi
+
+cd $WORKDIR
+
+if [ -f $5 ]; then
+    pip install -r $5
+fi # [ -f $5 ]
+
+
+
+# if [[ "$@" == "" ]]; then
+pyinstaller --clean -y --dist ./dist/windows --workpath /tmp $SPEC_FILE
+chown -R --reference=. ./dist/windows
+# else
+    # sh -c "$@"
+# fi # [[ "$@" == "" ]]
